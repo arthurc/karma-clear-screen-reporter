9b07de8f54007f842ca78e0631e25b002ac8aa9a
Wictor Chaves <wictor@hotmail.com.br>
2020-12-29 10:24:38 -0300

    Minor visual adjustments
    - Time the test was performed.
    - Visual improvements.

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-function ClearScreenReporter() {
-  this.onRunStart = function() {
+function breakLine() {
+    console.log('\033[2J');
+}
+
+function setCursor() {
     console.log("\u001b[2J\u001b[0;0H");
-  };
+}
+
+function msg(msg) {
+    var line = '\t--------------------------------------';
+    console.log('');
+    console.log(line);
+    console.log('\t\t' + msg);
+    console.log(line);
+    console.log('');
+    console.log('');
+}
+
+function ClearScreenReporter() {
+    this.onRunStart = function () {
+        breakLine();
+        setCursor();
+        msg('tests running ' + new Date().toLocaleTimeString());
+    };
 }
 
 module.exports = {
-  'reporter:clear-screen': ['type', ClearScreenReporter]
+    'reporter:clear-screen': ['type', ClearScreenReporter]
 };
